f95806ee2083c354ddba89d2b157061965602dd1
rkota(Rosaire Kota) <rosaire@kadea.co>
2023-11-10 09:32:41 +0100

    refactor:update login form

--- components/auth/Login.tsx
+++ components/auth/Login.tsx
@@ -102,7 +102,7 @@ export const Login = () => {
         </div>
       </section>
 
-      <section className="bg-primary-900 relative">
+      <section className="bg-primary-900 relative isolate">
         <div className="absolute left-20 top-1/2 -translate-y-1/2 sm:left-1/2 sm:-translate-x-1/2">
           <svg viewBox="0 0 558 558" width="558" height="558" fill="none" aria-hidden="true"
             className="animate-rotating">
@@ -117,6 +117,20 @@ export const Login = () => {
               stroke="#fff"></path>
             <path d="M1 279C1 125.465 125.465 1 279 1" stroke="url(#:S4:)" stroke-linecap="round"></path>
           </svg>
+        </div>
+         <div
+          aria-hidden="true"
+          className=" pointer-events-none absolute inset-x-0 top-60 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+        >
+          <div
+            style={{
+              clipPath:
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+            }}
+            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w[36.125rem]
+             -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-primary-800
+             sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]  opacity-30"
+          />
         </div>
         <div className="hidden lg:flex justify-center my-3">
           {/* <Image
