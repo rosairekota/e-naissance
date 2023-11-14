9c8f46ace6450ee84a87ff6d4bfa5e49c3baa265
rkota(Rosaire Kota) <rosaire@kadea.co>
2023-11-14 19:55:52 +0100

    refactor:remove google cta

--- components/auth/Signup.tsx
+++ components/auth/Signup.tsx
@@ -131,22 +131,6 @@ const SignUp: React.FC = () => {
             </form>
 
             <div className="mt-3 space-y-3">
-              <button
-                type="button"
-                className="relative inline-flex items-center justify-center w-full px-4 py-4 text-primary-800 font-semibold text-gray-700 transition-all duration-200 bg-transparent border-2 border-primary-800 rounded-md hover:bg-gray-100 focus:bg-gray-100 hover:text-primary-900 focus:text-black focus:outline-none"
-              >
-                <div className="absolute inset-y-0 left-0 p-4">
-                  <svg
-                    className="w-6 h-6 text-primary-800"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="currentColor"
-                  >
-                    <path d="M20.283 10.356h-8.327v3.451h4.792c-.446 2.193-2.313 3.453-4.792 3.453a5.27 5.27 0 0 1-5.279-5.28 5.27 5.27 0 0 1 5.279-5.279c1.259 0 2.397.447 3.29 1.178l2.6-2.599c-1.584-1.381-3.615-2.233-5.89-2.233a8.908 8.908 0 0 0-8.934 8.934 8.907 8.907 0 0 0 8.934 8.934c4.467 0 8.529-3.249 8.529-8.934 0-.528-.081-1.097-.202-1.625z"></path>
-                  </svg>
-                </div>
-                Créer compte avec Google
-              </button>
 
               <div className="mt-2 text-base text-gray-600 ">
                 Vous avez déjà un compte ?{" "}
