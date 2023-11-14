b459ae2bdca737a8c60c421562974130d3faddeb
rkota(Rosaire Kota) <rosaire@kadea.co>
2023-11-14 20:09:23 +0100

    fix:authOptions is not a valid Route issue

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,7 +1,7 @@
 import { Dashboard } from "@/components/admin/Dashboard";
 import { Metadata } from "next";
 import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/route";
+
 
 export const metadata: Metadata = {
   title: "SwyftPay",
