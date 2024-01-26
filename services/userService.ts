f51d8d069615645c52720572156684e0ddf2332c
rkota(Rosaire Kota) <rosairekota@anicns.gouv.cd>
2024-01-26 01:23:02 +0100

    fix:resolve api url issue

--- services/userService.ts
+++ services/userService.ts
@@ -1,20 +1,9 @@
-import { SHOPBUY_API_URL } from "@/config"
+
 import { USSD_API_URL, fetchUssdApiWithToken } from "@/config/api-urls"
 import { IAuth } from "@/types/auth"
 import { getToken, hashData } from "./global"
 
-export const fetchUserMe = async (token: string)=>{
-    try {
-      const res = await fetch(`${SHOPBUY_API_URL}/users/me`, {
-        method: 'GET',
-        headers: { "Content-Type": "application/json" , 'Authorization': `Bearer ${token}`}
-      })
-      const result = await res.json()
-      return  result
-    }catch{
-      console.error('Impossible de récupérer les informations de l\'utilisateur')
-    }
-  }
+
 
   export const loginUser = async (credentials: IAuth)=>{
     try {
